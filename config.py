e70ed4683e484ac8a5cdcc644389cbd356ffb4a4
kingak971916 <kingak971916@gmail.com>
2026-03-08 18:48:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,14 +38,14 @@ THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/WpKRVMKy/7168219724-28
 
 
 # # Bot configuration
-# API_ID = int(os.environ.get("API_ID", "21157244"))
-# API_HASH = os.environ.get("API_HASH", "4981c2699bd91c7db836ec8f77e5b0f0")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-# BOT_USERNAME = os.environ.get("BOT_USERNAME", "Gajjubhaikaext_bot")
-# OWNER_ID = int(os.environ.get("OWNER_ID", "1783306092"))
-# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1783306092").split()))
-# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002195125473"))
-# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002195125473"))
-# THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
+# API_ID = int(os.environ.get("API_ID", "34724970"))
+# API_HASH = os.environ.get("API_HASH", "f240eae7c60e8e30c17203ab0e052f7e")
+# BOT_TOKEN = os.environ.get("BOT_TOKEN", "8707483940:AAEUgZV01kAI3Ef9ArQl2clWeVSnoeCAQcs")
+# BOT_USERNAME = os.environ.get("BOT_USERNAME", "Ak_course_extractor_bot")
+# OWNER_ID = int(os.environ.get("OWNER_ID", "7521421400"))
+# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7521421400").split()))
+# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "1003758676689"))
+# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
+# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003515041061"))
+# THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/WpKRVMKy/7168219724-28094.jpg")
 
